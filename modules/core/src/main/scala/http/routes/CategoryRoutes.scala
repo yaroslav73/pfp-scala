c7e66fcace9e36fa177f3a889fcd7c27a82f2f3c
Yaroslav Sydorenko <ysdyorenko@evolution.com>
2022-09-09 17:11:52 +0300

    Add categories routes.

---
+++ modules/core/src/main/scala/http/routes/CategoryRoutes.scala
@@ -0,0 +1,18 @@
+package http.routes
+
+import cats.Monad
+import domain.Categories
+import org.http4s.HttpRoutes
+import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
+import org.http4s.dsl.Http4sDsl
+import org.http4s.server.Router
+
+final case class CategoryRoutes[F[_]: Monad](categories: Categories[F]) extends Http4sDsl[F] {
+  private[routes] val prefixPath = "/categories"
+
+  private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
+    case GET -> Root => Ok(categories.findAll)
+  }
+
+  val routes: HttpRoutes[F] = Router(prefixPath -> httpRoutes)
+}
