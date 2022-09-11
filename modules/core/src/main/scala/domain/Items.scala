00e94e0045b0998fe619ffcc39af17bf81245943
Yaroslav Sydorenko <ysdyorenko@evolution.com>
2022-09-11 14:13:46 +0300

    Add Admin Item routes.

--- modules/core/src/main/scala/domain/Items.scala
+++ modules/core/src/main/scala/domain/Items.scala
@@ -1,9 +1,13 @@
 package domain
 
-import domain.Brands.{Brand, BrandId}
-import domain.Categories.{Category, CategoryId}
-import io.circe.Encoder
+import domain.Brands.{ Brand, BrandId }
+import domain.Categories.{ Category, CategoryId }
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.string.{ Uuid, ValidBigDecimal }
+import eu.timepit.refined.types.string.NonEmptyString
+import io.circe.{ Decoder, Encoder }
 import io.circe.generic.semiauto.deriveEncoder
+import io.circe.refined.{ refinedDecoder, refinedEncoder }
 import squants.market._
 
 import java.util.UUID
@@ -21,7 +25,6 @@ object Items {
     brand: Brand,
     category: Category
   )
-
   object Item {
     implicit val itemDescriptionEncoder: Encoder[ItemDescription] = deriveEncoder[ItemDescription]
     implicit val itemNameEncoder: Encoder[ItemName]               = deriveEncoder[ItemName]
@@ -29,7 +32,51 @@ object Items {
     implicit val itemEncoder: Encoder[Item]                       = deriveEncoder[Item]
   }
 
-  case class CreateItem(
+  final case class ItemNameParam(value: NonEmptyString)
+  object ItemNameParam {
+    implicit val jsonEncoder: Encoder[ItemNameParam] =
+      Encoder.forProduct1("name")(_.value)
+
+    implicit val jsonDecoder: Decoder[ItemNameParam] =
+      Decoder.forProduct1("name")(ItemNameParam.apply)
+  }
+
+  final case class ItemDescriptionPram(value: NonEmptyString)
+  object ItemDescriptionPram {
+    implicit val jsonEncoder: Encoder[ItemDescriptionPram] =
+      Encoder.forProduct1("name")(_.value)
+
+    implicit val jsonDecoder: Decoder[ItemDescriptionPram] =
+      Decoder.forProduct1("name")(ItemDescriptionPram.apply)
+  }
+
+  final case class PriceParam(value: String Refined ValidBigDecimal)
+  object PriceParam {
+    implicit val jsonEncoder: Encoder[PriceParam] =
+      Encoder.forProduct1("name")(_.value)
+
+    implicit val jsonDecoder: Decoder[PriceParam] =
+      Decoder.forProduct1("name")(PriceParam.apply)
+  }
+
+  final case class CreateItemParam(
+    name: ItemNameParam,
+    description: ItemDescriptionPram,
+    price: PriceParam,
+    brandId: BrandId,
+    categoryId: CategoryId,
+  ) {
+    def toDomain: CreateItem =
+      CreateItem(
+        ItemName(name.value.value),
+        ItemDescription(description.value.value),
+        USD(BigDecimal(price.value.value)),
+        brandId,
+        categoryId
+      )
+  }
+
+  final case class CreateItem(
     name: ItemName,
     description: ItemDescription,
     price: Money,
@@ -37,5 +84,18 @@ object Items {
     categoryId: CategoryId
   )
 
-  case class UpdateItem(itemId: ItemId, price: Money)
+  final case class ItemIdParam(value: String Refined Uuid)
+  object ItemIdParam {
+    implicit val jsonEncoder: Encoder[ItemIdParam] =
+      Encoder.forProduct1("name")(_.value)
+
+    implicit val jsonDecoder: Decoder[ItemIdParam] =
+      Decoder.forProduct1("name")(ItemIdParam.apply)
+  }
+
+  final case class UpdateItemParam(id: ItemIdParam, price: PriceParam) {
+    def toDomain: UpdateItem = UpdateItem(ItemId(UUID.fromString(id.value.value)), USD(BigDecimal(price.value.value)))
+  }
+
+  final case class UpdateItem(itemId: ItemId, price: Money)
 }
---
+++ modules/core/src/main/scala/http/routes/admin/AdminItemRoutes.scala
@@ -0,0 +1,37 @@
+package http.routes.admin
+
+import cats.MonadThrow
+import cats.implicits.{ catsSyntaxFlatMapOps, toFlatMapOps }
+import domain.Items.{ CreateItemParam, UpdateItemParam }
+import http.auth.Users.AdminUser
+import http.routes.RefinedRequestDecoder
+import io.circe.JsonObject
+import io.circe.generic.codec.DerivedAsObjectCodec.deriveCodec
+import io.circe.syntax.EncoderOps
+import org.http4s.{ AuthedRoutes, HttpRoutes }
+import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
+import org.http4s.circe.JsonDecoder
+import org.http4s.dsl.Http4sDsl
+import org.http4s.server.{ AuthMiddleware, Router }
+import services.Items
+
+final case class AdminItemRoutes[F[_]: JsonDecoder: MonadThrow](items: Items[F]) extends Http4sDsl[F] {
+  private[routes] val prefixPath = "/items"
+
+  private val httpRoutes: AuthedRoutes[AdminUser, F] = AuthedRoutes.of {
+    case ar @ POST -> Root as _ =>
+      ar.req.decoderR[CreateItemParam] { item =>
+        items.create(item.toDomain).flatMap { id =>
+          Created(JsonObject.singleton("item_id", id.asJson))
+        }
+      }
+
+    case ar @ PUT -> Root as _ =>
+      ar.req.decoderR[UpdateItemParam] { item =>
+        items.update(item.toDomain) >> Ok()
+      }
+  }
+
+  def routes(authMiddleware: AuthMiddleware[F, AdminUser]): HttpRoutes[F] =
+    Router(prefixPath -> authMiddleware(httpRoutes))
+}
