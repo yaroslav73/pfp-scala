6ae698f545b2375b2a48ee0655ed58ba45e4a111
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2022-09-23 12:49:29 +0300

    Add implementation for JwtExpire and JwtClock.

--- modules/core/src/main/scala/auth/JwtExpire.scala
+++ modules/core/src/main/scala/auth/JwtExpire.scala
@@ -1,8 +1,21 @@
 package auth
 
+import cats.effect.kernel.Sync
+import cats.implicits.toFunctorOps
 import config.Types.TokenExpiration
+import effects.JwtClock
 import pdi.jwt.JwtClaim
 
 trait JwtExpire[F[_]] {
   def expiresIn(claim: JwtClaim, expiration: TokenExpiration): F[JwtClaim]
+}
+
+object JwtExpire {
+  def make[F[_]: Sync]: F[JwtExpire[F]] =
+    JwtClock[F].utc.map { implicit jwtClock =>
+      new JwtExpire[F] {
+        def expiresIn(claim: JwtClaim, expiration: TokenExpiration): F[JwtClaim] =
+          Sync[F].delay(claim.issuedNow.expiresIn(expiration.value.toMillis))
+      }
+    }
 }
